5e6d069a0db522ceff86c93816a3bf8841de2272
genxium <genxium@hotmail.com>
2020-05-28 12:51:58 +0800

    A commit for a "just became working mysql innodb cluster" example.

---
+++ database/mysql/docker/README.md
@@ -0,0 +1,48 @@
+Exemplifying the configuration.
+
+We'd like to have `mysqlsh> dba.configureInstance("root@192.168.56.102:3307")` to report 
+```
+This instance reports its own address as 192.168.56.102:3307
+```
+which is an ACCESSIBLE ADDR for the "mysqlclient on `192.168.56.102:3308` a.k.a. the other `DockerContainer`", where
+- "192.168.56.102" is the "ipaddr of an `NetworkIface` other than `docker0` or `docker-gwbridge` on the `DockerHostOS`", 
+- ":3307" and "::3308" are used by 2 different "DockerContainer"s on that "DockerHostOS".  
+
+Moreover, The subnet "192.168.56.0/24" could be provided by 
+```
+"a `NetworkIface of Ubuntu within VirtualBox(in turn running on Windows), e.g. enp0s8` from the `Host-only adapter of VirtualBox`"
+```
+, and is a common accessible network for all
+- "mysqlsh" process running on "Windows",
+- both "DockerContainer"s of "mysqld" running on "Ubuntu within VirtualBox(in turn running on Windows)".
+
+In this case, your "ipaddr of `NetworkIface`==`docker0` on the `DockerHostOS`" might be "172.17.0.1" and that the 2 different "DockerContainer"s possess "172.17.0.3" & "172.17.0.4" respectively. The communication between "DockerContainer"s MIGHT NOT go via this `NetworkIface`==`docker0`(except that it's the default route for destination=="192.168.56.102" in certain cases). 
+
+```
+Gitbash> mysqlsh
+MySQL  JS > \connect --mysql root@192.168.56.102:3307
+MySQL  192.168.56.102:3307 ssl  JS > dba.checkInstanceConfiguration()
+MySQL  192.168.56.102:3307 ssl  JS > dba.dropMetadataSchema() // If a the current session was previously added to any cluster.
+MySQL  192.168.56.102:3307 ssl  JS > dba.configureInstance()
+MySQL  192.168.56.102:3307 ssl  JS > var mycluster = dba.createCluster('mycluster') 
+# You might call "STOP SLAVE; RESET SLAVE" by the "mysqlclient on 192.168.56.102:3307" if "[ERROR] Slave SQL for channel 'group_replication_applier': Slave failed to initialize relay log info structure from the repository, Error_code: 1872" shows up spuriously. See https://github.com/vitessio/vitess/issues/5067 for a relevant discussion.
+
+MySQL  192.168.56.102:3307 ssl  JS > mycluster.setupAdminAccount('myclusterAdmin')
+MySQL  192.168.56.102:3307 ssl  JS > \connect myclusterAdmin@192.168.56.102:3307
+
+MySQL  192.168.56.102:3307 ssl  JS > dba.configureInstance("root@192.168.56.102:3308")
+MySQL  192.168.56.102:3307 ssl  JS > mycluster.checkInstanceState("root@192.168.56.102:3308")
+MySQL  192.168.56.102:3307 ssl  JS > mycluster.addInstance("root@192.168.56.102:3308")
+```
+
+## An alternative "--report-host" configuration.
+
+If "mysqld --report-host=<DockerContainerId>" were used, a "<DockerContainerId>" would be resolved to "172.17.0.3" or "172.17.0.4" by the "mysqld" process on each respective "<DockerContainer>", check
+```
+docker logs -f <DockerContainerId>" 
+```
+upon calling
+```
+mysqlsh> dba.createCluster(...)
+```
+for verification if such "--report-host" were expected.
--- database/mysql/docker/launch_databases_docker_container.sh
+++ database/mysql/docker/launch_databases_docker_container.sh
@@ -7,14 +7,20 @@ if [ $# -ne 1 ]; then
 fi
 
 unifiedHostname=$1
+mysqldVersion=8.0 # Deliberately using 8.0+ instead of 5.7- here, because since 8.0+ the mysqld comes with a "MySQL Clone Plugin(https://dev.mysql.com/doc/refman/8.0/en/clone-plugin.html)" which together with "mysqlsh" significantly simplifies "Group Replication" configuring. See "README" for an example sequence of commands on "mysqlsh".  
 
-# docker pull mysql:5.7
+# docker pull mysql:$mysqldVersion
 ####
 # ```
 # When you start the MySQL Docker container, you can pass configuration options to the server through the docker run command. For example:". 
 # -- reference https://dev.mysql.com/doc/refman/5.7/en/docker-mysql-more-topics.html#docker-configuring-server 
 # ```
-commonMysqldOptions="--gtid-mode=ON --enforce-gtid-consistency=ON --binlog-checksum=NONE --log-bin --log-slave-updates=ON --master-info-repository=TABLE --relay-log-info-repository=TABLE --transaction-write-set-extraction=XXHASH64 --report-host=$unifiedHostname"
+####
+
+####
+# References for what mysqld options to add for "MySQL Group Replication" https://dev.mysql.com/doc/refman/5.7/en/group-replication-configuring-instances.html#group-replication-configure-replication-framework.
+####
+commonMysqldOptions="--gtid-mode=ON --enforce-gtid-consistency=ON --binlog-checksum=NONE --log-bin=binlog --binlog-format=ROW --log-slave-updates=ON --master-info-repository=TABLE --relay-log-info-repository=TABLE --transaction-write-set-extraction=XXHASH64"
 
 commonDockerContainerEnvs="-e MYSQL_ROOT_HOST=% -e MYSQL_ALLOW_EMPTY_PASSWORD=yes"
 
@@ -26,7 +32,8 @@ portOnContainer1=3307
 # ```
 # docker exec <DockerContainerId> mysql -e "SHOW GLOBAL VARIABLES LIKE 'PORT';"
 # ``` 
-cmd1="docker run $commonDockerContainerEnvs -d -p :$port1OnHostOS:$portOnContainer1 --mount 'type=volume,src=shared_mysql_datadir_base_1,dst=/var/lib/mysql' mysql:5.7 --server-id=1 --report-port=$port1OnHostOS --port=$portOnContainer1 $commonMysqldOptions"
+#cmd1="docker run $commonDockerContainerEnvs -d -p :$port1OnHostOS:$portOnContainer1 --mount 'type=volume,src=shared_mysql_datadir_base_1,dst=/var/lib/mysql' mysql:$mysqldVersion --server-id=1 --report-port=$port1OnHostOS --port=$portOnContainer1 $commonMysqldOptions"
+cmd1="docker run $commonDockerContainerEnvs -d --net=host --mount 'type=volume,src=shared_mysql_datadir_base_1,dst=/var/lib/mysql' mysql:$mysqldVersion --server-id=1 --report-host=$unifiedHostname --report-port=$port1OnHostOS --port=$portOnContainer1 $commonMysqldOptions"
 echo $cmd1
 #eval $cmd1
 
@@ -34,34 +41,9 @@ echo $cmd1
 # Can test by a mysql-client on the HostOS, e.g. "mysql --host <ip of HostOS> --port 3308 -uroot". 
 port2OnHostOS=3308
 portOnContainer2=3308
-cmd2="docker run -d $commonDockerContainerEnvs -p :$port2OnHostOS:$portOnContainer2 --mount 'type=volume,src=shared_mysql_datadir_base_2,dst=/var/lib/mysql' mysql:5.7 --server-id=2 --report-port=$port2OnHostOS --port=$portOnContainer2 $commonMysqldOptions"
+#cmd2="docker run -d $commonDockerContainerEnvs -p :$port2OnHostOS:$portOnContainer2 --mount 'type=volume,src=shared_mysql_datadir_base_2,dst=/var/lib/mysql' mysql:$mysqldVersion --server-id=2 --report-port=$port2OnHostOS --port=$portOnContainer2 $commonMysqldOptions"
+cmd2="docker run -d $commonDockerContainerEnvs --net=host --mount 'type=volume,src=shared_mysql_datadir_base_2,dst=/var/lib/mysql' mysql:$mysqldVersion --server-id=2 --report-host=$unifiedHostname --report-port=$port2OnHostOS --port=$portOnContainer2 $commonMysqldOptions"
 echo $cmd2
 #eval $cmd2
 
-################
-# Exemplifying the configuration.
-#
-# We'd like to have `mysqlsh> dba.configureInstance("root@192.168.56.102:3307")` to report 
-# ```
-# This instance reports its own address as 192.168.56.102:3307
-# ```
-# which is an ACCESSIBLE ADDR for the "mysqlclient on `192.168.56.102:3308` a.k.a. the other `DockerContainer`" due to "overlaying", where
-# - "192.168.56.102" is the "ipaddr of an `NetworkIface` other than `docker0` or `docker-gwbridge` on the `DockerHostOS`", 
-# - ":3307" and "::3308" are used by 2 different "DockerContainer"s on that "DockerHostOS".  
-#
-# In this case, your "ipaddr of `NetworkIface`==`docker0` on the `DockerHostOS`" might be "172.17.0.1" and that the 2 different "DockerContainer"s possess "172.17.0.3" & "172.17.0.4" respectively. The communication between "DockerContainer"s MIGHT NOT go via this `NetworkIface`==`docker0`(except that it's the default route for destination=="192.168.56.102" in certain cases). 
-################
 
-################
-# An alternative "--report-host" configuration.
-#
-# If "mysqld --report-host=<DockerContainerId>" were used, a "<DockerContainerId>" would be resolved to "172.17.0.3" or "172.17.0.4" by the "mysqld" process on each respective "<DockerContainer>", check
-# ```
-# docker logs -f <DockerContainerId>" 
-# ```
-# upon calling
-# ```
-# mysqlsh> dba.createCluster(...)
-# ```
-# for verification if such "--report-host" were expected.
-###############

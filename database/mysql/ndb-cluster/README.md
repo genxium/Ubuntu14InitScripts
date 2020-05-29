c5791876acf1394a4863025d7835ae986cbd92bc
genxium <genxium@hotmail.com>
2020-05-29 23:34:28 +0800

    Updated "database/mysql/ndb-cluster".

--- database/mysql/ndb-cluster/README.md
+++ database/mysql/ndb-cluster/README.md
@@ -1,3 +1,38 @@
 Using DockerImage https://hub.docker.com/r/mysql/mysql-cluster/ built from https://github.com/mysql/mysql-docker/tree/mysql-cluster.
 
 All "DockerContainer"s in this example will use "--net=host", such that `inter-process & inter-OS ipaddr routing` is handled by "DockerHostOS", as explained in https://github.com/genxium/Ubuntu14InitScripts/tree/master/database/mysql/docker-innodb-cluster.
+
+# To verify that we're using "instances of NDBEngine w/ `ndbd`"
+The following steps work whether or not each "DockerContainer" has a mounted volume. 
+
+- docker exec -it mysql1 mysql -uroot
+```
+/*Data example from https://dev.mysql.com/doc/mysql-cluster-excerpt/5.7/en/mysql-cluster-install-example-data.html.*/ 
+
+CREATE DATABASE test
+USE test
+CREATE TABLE `City` (   `ID` int(11) NOT NULL auto_increment,   `Name` char(35) NOT NULL default '',   `CountryCode` char(3) NOT NULL default '',   `District` char(20) NOT NULL default '',   `Population` int(11) NOT NULL default '0',   PRIMARY KEY  (`ID`) ) ENGINE=NDBCLUSTER DEFAULT CHARSET=latin1;
+INSERT INTO `City` VALUES (1,'Kabul','AFG','Kabol',1780000); 
+INSERT INTO `City` VALUES (2,'Qandahar','AFG','Qandahar',237500); 
+INSERT INTO `City` VALUES (3,'Herat','AFG','Herat',186800);
+
+SELECT * FROM City;
+
+/*Should succeed in obtaining data.*/ 
+```
+- docker stop ndb1
+- docker stop ndb2
+- docker exec -it mysql1 mysql -uroot
+```
+SELECT * FROM City;
+
+/*Should fail to obtain data.*/ 
+```
+- docker start ndb1
+- docker start ndb2
+- docker exec -it mysql1 mysql -uroot
+```
+SELECT * FROM City;
+
+/*Should succeed in obtaining data.*/ 
+```
--- database/mysql/ndb-cluster/launch_databases_docker_container.sh
+++ database/mysql/ndb-cluster/launch_databases_docker_container.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
 dockerNetworkName=ndb_cluster
-mysqlClusterVersion=8.0
+mysqlClusterVersion=latest
 dockerImageFullname=mysql/mysql-cluster:$mysqlClusterVersion
 
 cmd1="docker network create $dockerNetworkName --subnet=192.168.0.0/16" 
 
-# Note that the default "configuration file(s) for `ndb_mgmd` in $dockerImageFullname assumes that `ndb_mgmd` runs in a subnet 192.168.0.0/16 and will bind to 192.168.0.2". See https://github.com/mysql/mysql-docker/blob/mysql-cluster/8.0/cnf/mysql-cluster.cnf for details.
-cmd2="docker run -d --net=$dockerNetworkName --name=management1 --ip=192.168.0.2 $dockerImageFullname ndb_mgmd"
-cmd3="docker run -d --net=$dockerNetworkName --name=ndb1 --ip=192.168.0.3 $dockerImageFullname ndbd"
-cmd4="docker run -d --net=$dockerNetworkName --name=ndb2 --ip=192.168.0.4 $dockerImageFullname ndbd"
+# Note that the default "configuration file(s) for `ndb_mgmd` in $dockerImageFullname assumes that `ndb_mgmd` runs in a subnet 192.168.0.0/16 and will bind to 192.168.0.2". See https://github.com/mysql/mysql-docker/blob/mysql-cluster/7.6/cnf/mysql-cluster.cnf for details.
+
+# The "datadir" of each "DockerContainer" is specified in https://github.com/mysql/mysql-docker/blob/mysql-cluster/7.6/cnf/mysql-cluster.cnf as well.
+#docker volume create shared_mysql_cluster_mgmd_datadir
+cmd2="docker run -d --net=$dockerNetworkName --name=management1 --mount 'type=volume,src=shared_mysql_cluster_mgmd_datadir,dst=/var/lib/mysql' --ip=192.168.0.2 $dockerImageFullname ndb_mgmd"
+
+#docker volume create shared_mysql_cluster_ndbd_datadir_1
+cmd3="docker run -d --net=$dockerNetworkName --name=ndb1 --mount 'type=volume,src=shared_mysql_cluster_ndbd_datadir_1,dst=/var/lib/mysql' --ip=192.168.0.3 $dockerImageFullname ndbd"
+#docker volume create shared_mysql_cluster_ndbd_datadir_2
+cmd4="docker run -d --net=$dockerNetworkName --name=ndb2 --mount 'type=volume,src=shared_mysql_cluster_ndbd_datadir_2,dst=/var/lib/mysql' --ip=192.168.0.4 $dockerImageFullname ndbd"
+
 cmd5="docker run -e MYSQL_ROOT_HOST=% -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d --net=$dockerNetworkName --name=mysql1 --ip=192.168.0.10 $dockerImageFullname mysqld"
+
+#####
+# If some named "DockerContainer"s are exited, and should be removed->rerun with new parameters, try 
+# ```
+# docker container rm $(docker container ls -a | grep Exited | awk '{print $1}') 
+# ```
+# to clean up.
+#####
 
 echo $cmd1
 echo $cmd2
 echo $cmd3
 echo $cmd4
 echo $cmd5
+
+cmd6="docker run -it --net=$dockerNetworkName mysql/mysql-cluster:$mysqlClusterVersion ndb_mgm"
+printf "\nUse the following command to connect to ndb_mgmd...\n$cmd6\n\n"
